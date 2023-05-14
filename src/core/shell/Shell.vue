eefd54c8157bbbaca7499918d88d06d49932835d
Maciej Walecki <maciejwalecki99@gmail.com>
2023-05-14 20:36:59 +0200

    feat: configured pinia and created todo feature

---
+++ src/core/shell/Shell.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import ProductList from "../../features/todo-list/TodoList.vue.js";
+</script>
+
+<template>
+    <h1 class="font-bold">Muscode App</h1>
+    <main class="grid-cols-3 gap-2">
+        <ProductList></ProductList>
+    </main>
+</template>
