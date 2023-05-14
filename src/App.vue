d0b3534b531f5d6a8568cf424d2180187e123319
Maciej Walecki <maciejwalecki99@gmail.com>
2023-05-14 20:36:28 +0200

    feat: added shell component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-
+  import Shell from "./core/shell/Shell.vue";
 </script>
 
 <template>
-  
+  <div class="min-h-screen w-full flex items-center justify-center">
+    <Shell></Shell>
+  </div>
 </template>
